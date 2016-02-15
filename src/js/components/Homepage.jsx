ad8bba69d98debad00777d69ed2bbd14975042ca
hobblestu <jonctuttle@gmail.com>
2016-02-14 19:22:48 -0600

    worked on the home page a little.

--- src/js/components/Homepage.jsx
+++ src/js/components/Homepage.jsx
@@ -17,11 +17,23 @@ module.exports = React.createClass({
     render() {
         return (
             <div className="homepage">
-                <div className="title">School Lunch Application</div>
-                <div>Please enter</div>
-                <Textfield label="First Name" onChange={(e) => this.setState({firstName: e.target.value})} />
-                <Textfield label="Last Name" onChange={(e) => this.setState({lastName: e.target.value})} />
-                <Button disabled={!this.state.firstName || !this.state.lastName} onClick={() => this.props.onGetStartedClick(this.state.firstName, this.state.lastName)}>Get Started</Button>
+                <div className="heroImage">
+                    <div className="image">
+                        <img src="hero.jpg"/>
+                    </div>
+                    <div className="heroContainer">
+                        <div className="title">School Lunch Application</div>
+                        <div className="intro"><p>Welcome to the </p></div>
+                    </div>
+                </div>
+                <div className="nameSelection">
+                    <div className="nameContainer">
+                        <p>To start please fill out your name below to start the application</p>
+                        <Textfield label="First Name" floatingLabel onChange={(e) => this.setState({firstName: e.target.value})} style={{width: '300px'}}/>
+                        <Textfield label="Last Name" floatingLabel onChange={(e) => this.setState({lastName: e.target.value})} style={{width: '300px', marginLeft:"30px"}} />
+                        <Button raised ripple disabled={!this.state.firstName || !this.state.lastName} onClick={() => this.props.onGetStartedClick(this.state.firstName, this.state.lastName)}>Get Started</Button>
+                    </div>
+                </div>
             </div>
         );
     }
