c76242f7dd2320193484c372ba740b2730651109
Garston Tremblay <garstont+github@gmail.com>
2016-02-15 15:07:44 -0700

    Refactor NumPeopleSelection page to get proper structure for styling

--- src/js/components/NumPeopleSelection.jsx
+++ src/js/components/NumPeopleSelection.jsx
@@ -19,35 +19,58 @@ module.exports = React.createClass({
             <div className="num-people-selection-page">
                 <div>Lets get started</div>
                 <div>Please start by selecting how many applicants and adults are in your household. Please select only those that share income and expenses.</div>
-                <Card shadow={1} style={{width: '1000px', margin: 'auto'}}>
-                    <CardTitle>Children</CardTitle>
-                    <CardText>Please select the number of children 18 and under and anyone who is over 18 and in grade 12 or below. A household is anyone who shares expenses and income.</CardText>
-                    <div className="iconContainer">
-                        { this.generateIconCounterRow('numChildren', 'baby.png') }
-                    </div>
-                </Card>
-                <Card shadow={1} style={{width: '1000px', margin: 'auto', marginTop: "20px"}}>
-                    <CardTitle>Adults</CardTitle>
-                    <CardText>Please select the number of adults in your household that share income and expenses.</CardText>
-                    <div className="iconContainer">
-                        { this.generateIconCounterRow('numAdults', 'adult.png') }
-                    </div>
-                    <Checkbox onChange={e => this.setState({areAdultsInAssistanceProgram: e.target.checked})} />Check if any adults are in the assistance programs SNAP, TANF, FDPIR
-                </Card>
+                { this.generateCard({
+                    header:'Children',
+                    helperText: 'Please select the number of children 18 and under and anyone who is over 18 and in grade 12 or below. A household is anyone who shares expenses and income.',
+                    imgSrc: 'baby.png',
+                    stateFieldName: 'numChildren'
+                }) }
+                { this.generateCard({
+                    additionalContent: (
+                        <div>
+                            <Checkbox onChange={e => this.setState({areAdultsInAssistanceProgram: e.target.checked})} />
+                            Check if any adults are in the assistance programs SNAP, TANF, FDPIR
+                        </div>
+                    ),
+                    header:'Adults',
+                    helperText: 'Please select the number of adults in your household that share income and expenses.',
+                    imgSrc: 'adult.png',
+                    stateFieldName: 'numAdults'
+                }) }
                 <Button onClick={() => navigateForward(this.state)}>Next</Button>
             </div>
         );
     },
 
-    generateIconCounterRow(stateFieldName, imgSrc) {
-        return _.map(_.range(8), index => {
+    generateCard({ additionalContent, header, helperText, imgSrc, stateFieldName }) {
+        var icons = _.map(_.range(8), index => {
             return <img
                 className={'icon' + (index + 1 > this.state[stateFieldName] ? ' not-selected' : '')}
                 key={`${stateFieldName}-icon-${index}`}
                 onClick={() => this.setState({ [stateFieldName]: index + 1 })}
                 src={imgSrc}
-            />
+            />;
+        });
 
-        }).concat(<div className="icon-counter" key={`${stateFieldName}-icon-counter`}>{ this.state[stateFieldName] }</div>);
+        return (
+            <Card shadow={1}>
+                <CardTitle>{header}</CardTitle>
+                <CardText>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>{ helperText }</td>
+                                <td/>
+                            </tr>
+                            <tr>
+                                <td>{ icons }</td>
+                                <td><div className="icon-counter" key={`${stateFieldName}-icon-counter`}>{ this.state[stateFieldName] }</div></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    { additionalContent }
+                </CardText>
+            </Card>
+        );
     }
 });
