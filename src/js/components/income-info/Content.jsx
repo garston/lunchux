1c10a210d0f8a70451be5ca6cb36441389d7f853
Garston Tremblay <garstont+github@gmail.com>
2016-02-19 23:25:46 -0700

    Adding adult cards with names only to Income Info

--- src/js/components/income-info/Content.jsx
+++ src/js/components/income-info/Content.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
-var { Button, Textfield } = require('react-mdl');
+var { Button, Card, CardText, Textfield } = require('react-mdl');
 var IncomeFrequencySelector = require('./IncomeFrequencySelector.jsx');
+var IconNamesTable = require('../general/IconNamesTable.jsx');
 var ApplicationStore = require('../../stores/ApplicationStore');
 var { navigateForward } = require('../../utils/ActionCreator');
+var { ADULT_ICON } = require('../../utils/Util');
 
 var grossIncomePattern = '^[0-9]+$';
 var grossIncomeRegEx = new RegExp(grossIncomePattern);
@@ -11,8 +13,9 @@ module.exports = React.createClass({
     displayName: 'IncomeInfoContent',
 
     getInitialState() {
-        var { applicantInfos, applicantIncomeInfos } = ApplicationStore.getFormData();
+        var { adultInfos, applicantInfos, applicantIncomeInfos, firstName, lastName, numAdults } = ApplicationStore.getFormData();
         return {
+            adultInfos: _.map(_.range(numAdults), index => (adultInfos && adultInfos[index]) || (index === 0 ? { firstName, lastName } : {})),
             applicantIncomeInfos: _.map(applicantInfos, (applicantInfo, index) => applicantInfo.receivesIncome && (applicantIncomeInfos ? applicantIncomeInfos[index] : {}))
         };
     },
@@ -21,12 +24,43 @@ module.exports = React.createClass({
         return (
             <div className="income-info-content">
                 { this._getApplicantIncomeSection() }
+                { this._getAdultInfoSection() }
                 <Button
-                    disabled={ _.some(this.state.applicantIncomeInfos, info => info && (!info.grossIncome || !grossIncomeRegEx.test(info.grossIncome) || !info.frequency)) }
+                    disabled={
+                        _.some(this.state.applicantIncomeInfos, info => info && (!info.grossIncome || !grossIncomeRegEx.test(info.grossIncome) || !info.frequency)) ||
+                        _.some(this.state.adultInfos, info => !info.firstName || !info.lastName)
+                    }
                     onClick={() => navigateForward(this.state)}
                 >
                     Next
                 </Button>
+            </div>
+        );
+    },
+
+    _getAdultInfoSection() {
+        var cards = _.map(this.state.adultInfos, (info, index) => {
+            return (
+                <Card key={ 'adult-card' + index } shadow={1}><CardText>
+                    <table><tbody><tr>
+                        <td>
+                            <IconNamesTable
+                                firstName={ info.firstName }
+                                icon={ ADULT_ICON }
+                                iconText={ index ? `Adult #${index + 1}` : 'You' }
+                                lastName={ info.lastName }
+                                onChange={(val, prop) => this._updateStateInfoArray(info, 'adultInfos', prop, val)}
+                            />
+                        </td>
+                    </tr></tbody></table>
+                </CardText></Card>
+            );
+        });
+
+        return (
+            <div>
+                Adult Information and Income
+                { cards }
             </div>
         );
     },
@@ -42,7 +76,7 @@ module.exports = React.createClass({
                             error="Please enter only numbers"
                             floatingLabel
                             label="Gross Income"
-                            onChange={e => this._updateApplicantIncomeInfo(applicantIncomeInfo, 'grossIncome', e.target.value)}
+                            onChange={e => this._updateStateInfoArray(applicantIncomeInfo, 'applicantIncomeInfos', 'grossIncome', e.target.value)}
                             pattern={ grossIncomePattern }
                             value={ applicantIncomeInfo.grossIncome }
                         />
@@ -50,7 +84,7 @@ module.exports = React.createClass({
                     <td>Every</td>
                     <td>
                         <IncomeFrequencySelector
-                            onChange={val => this._updateApplicantIncomeInfo(applicantIncomeInfo, 'frequency', val)}
+                            onChange={val => this._updateStateInfoArray(applicantIncomeInfo, 'applicantIncomeInfos', 'frequency', val)}
                             value={ applicantIncomeInfo.frequency }
                         />
                     </td>
@@ -66,9 +100,9 @@ module.exports = React.createClass({
         );
     },
 
-    _updateApplicantIncomeInfo(applicantIncomeInfo, key, val) {
+    _updateStateInfoArray(infoToUpdate, stateKey, key, val) {
         this.setState({
-            applicantIncomeInfos: _.map(this.state.applicantIncomeInfos, info => info === applicantIncomeInfo ? _.assign({}, info, { [key]: val }) : info)
+            [stateKey]: _.map(this.state[stateKey], info => info === infoToUpdate ? _.assign({}, info, { [key]: val }) : info)
         });
     }
 });
