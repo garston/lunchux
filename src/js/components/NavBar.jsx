01d306308434aa97a7f0da91e4792f1289dfe2a7
Garston Tremblay <garstont+github@gmail.com>
2016-02-24 19:31:38 -0700

    Changing NavBar icons to use Badge

--- src/js/components/NavBar.jsx
+++ src/js/components/NavBar.jsx
@@ -1,50 +1,46 @@
 var _ = require('lodash');
 var React = require('react');
+var { Badge } = require('react-mdl');
 var ApplicationStore = require('../stores/ApplicationStore');
 var { navigateTo } = require('../utils/ActionCreator');
 var { ADULT_ICON, CHILD_ICON, STEPS } = require('../utils/Util');
-var { Badge } = require('react-mdl');
 
 module.exports = React.createClass({
     displayName: 'NavBar',
 
     render() {
+        var { numAdults, numChildren } = ApplicationStore.getFormData();
         var segments = _(ApplicationStore.getVisitedSteps()).map(step => {
             switch(step){
                 case STEPS.APPLICANT_INFO:
                     return [
-
-                        this._generateStepCell('Applicant Information', step),
-                        this._generateStepCell(this._generateIcons(ApplicationStore.getFormData().numChildren), step)
+                        this._generatePageStep('Applicant Information', step),
+                        this._generateIconStep(numChildren, CHILD_ICON, step)
                     ];
                 case STEPS.ASSISTANCE_PROGRAM:
-                    return this._generateStepCell('Assistance Program', step);
+                    return this._generatePageStep('Assistance Program', step);
                 case STEPS.INCOME_INFO:
                     return [
-                        this._generateStepCell('Income and Adult Information', step),
-                        this._generateStepCell(this._generateIcons(ApplicationStore.getFormData().numAdults), step)
+                        this._generatePageStep('Income and Adult Information', step),
+                        this._generateIconStep(numAdults, ADULT_ICON, step)
                     ];
                 case STEPS.NUM_PEOPLE:
-                    return this._generateStepCell('Applicant and Adults', step);
-
+                    return this._generatePageStep('Applicant and Adults', step);
             }
         }).flatten().value();
 
         return <div className="nav-bar"><table><tbody><tr>{ segments }</tr></tbody></table></div>;
     },
 
-    _generateIcons(num) {
-        return _.map(_.range(num), index => <Badge text="1">index</Badge>);
+    _generateIconStep(num, imgSrc, step) {
+        return <td className='icon-step' key={ `${step}-icons` }><img src={ imgSrc } /><Badge text={ num }>&nbsp;</Badge></td>;
     },
 
-    _generateStepCell(content, step) {
-        var props = _.isString(content) ? {
+    _generatePageStep(content, step) {
+        var props = {
             className: `page-step${step === ApplicationStore.getStep() ? ' active-step' : ''}`,
             key: step,
             onClick: () => navigateTo(step)
-        } : {
-            className: 'icon-step',
-            key: `${step}-icons`
         };
         return <td { ...props }>{ content }</td>;
     }
