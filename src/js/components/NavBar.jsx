3b6a9f5542745acc83cd5a03aa010d4629787c78
Garston Tremblay <garstont+github@gmail.com>
2016-02-27 22:08:29 -0700

    Small NavBar cleanup

--- src/js/components/NavBar.jsx
+++ src/js/components/NavBar.jsx
@@ -4,26 +4,25 @@ var { Paper} = require('material-ui');
 var { Badge, Icon, Button,Dialog, DialogTitle, DialogContent,DialogActions  } = require('react-mdl');
 var ApplicationStore = require('../stores/ApplicationStore');
 var { navigateTo } = require('../utils/ActionCreator');
-var { ADULT_ICON, CHILD_ICON, STEPS } = require('../utils/Util');
+var { STEPS } = require('../utils/Util');
 
 module.exports = React.createClass({
     displayName: 'NavBar',
-
 
     render() {
         var { numAdults, numChildren } = ApplicationStore.getFormData();
         var segments = _(ApplicationStore.getVisitedSteps()).map(step => {
             switch(step){
                 case STEPS.APPLICANT_INFO:
-                    return this._generateIconStep('Children', step, numChildren, CHILD_ICON);
+                    return this._generateStepCell('Children', step, numChildren);
                 case STEPS.ASSISTANCE_PROGRAM:
-                    return this._generatePageStep('Assistance Program', step);
+                    return this._generateStepCell('Assistance Program', step);
                 case STEPS.INCOME_INFO:
-                    return this._generateIconStep('Income and Adults', step, numAdults, ADULT_ICON);
+                    return this._generateStepCell('Income and Adults', step, numAdults);
                 case STEPS.NUM_PEOPLE:
-                    return this._generatePageStep('Household Member Selection', step);
+                    return this._generateStepCell('Household Member Selection', step);
                 case STEPS.REVIEW_SUBMIT:
-                    return this._generatePageStep('Review / Submit', step);
+                    return this._generateStepCell('Review / Submit', step);
             }
         }).flatten().value();
 
@@ -31,21 +30,13 @@ module.exports = React.createClass({
                 <div className="navSection"><table><tbody><tr>{ segments }</tr></tbody></table></div></div></Paper>;
     },
 
-    _generateIconStep(content, step, num, imgSrc) {
-        var props = {
+    _generateStepCell(content, step, num) {
+        var tdProps = {
             className: `page-step${step === ApplicationStore.getStep() ? ' active-step' : ''}`,
             key: step,
             onClick: () => navigateTo(step)
         };
-        return <td { ...props }>{ content }<Badge text={ num } >&nbsp;</Badge></td>;
-    },
-
-    _generatePageStep(content, step) {
-        var props = {
-            className: `page-step${step === ApplicationStore.getStep() ? ' active-step' : ''}`,
-            key: step,
-            onClick: () => navigateTo(step)
-        };
-        return <td { ...props }>{ content }</td>;
+        var badge = _.isNumber(num) && <Badge text={ num } >&nbsp;</Badge>;
+        return <td { ...tdProps }>{ content }{ badge }</td>;
     }
 });
