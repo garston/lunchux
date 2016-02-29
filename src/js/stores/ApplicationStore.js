41f21494f69f815dce3730667c1d0b272962f5c0
Garston Tremblay <garstont+github@gmail.com>
2016-02-28 21:25:01 -0700

    Remove previously reachable form data when skipping pages during flowchart logic

--- src/js/stores/ApplicationStore.js
+++ src/js/stores/ApplicationStore.js
@@ -9,7 +9,7 @@ var formData = {};
 var visitedSteps = [STEPS.HOME];
 
 var getCurrentStep = () => _.last(visitedSteps);
-var getNextStep = () => {
+var moveToNextStep = () => {
     switch(getCurrentStep()) {
         case STEPS.HOME:
             return STEPS.NUM_PEOPLE;
@@ -17,10 +17,13 @@ var getNextStep = () => {
             return STEPS.APPLICANT_INFO;
         case STEPS.APPLICANT_INFO:
             if(formData.areAdultsInAssistanceProgram) {
+                formData = _.omit(formData, ['adultInfos', 'applicantIncomeInfos']);
                 return STEPS.ASSISTANCE_PROGRAM;
             }else if(_.every(formData.applicantInfos, info => info.fosterChild)) {
+                formData = _.omit(formData, ['adultInfos', 'applicantIncomeInfos', 'assistanceProgram', 'assistanceProgramNumber']);
                 return STEPS.REVIEW_SUBMIT;
             }
+            formData = _.omit(formData, ['assistanceProgram', 'assistanceProgramNumber']);
             return STEPS.INCOME_INFO;
         case STEPS.ASSISTANCE_PROGRAM:
         case STEPS.INCOME_INFO:
@@ -51,7 +54,7 @@ ApplicationStore.dispatchToken = Dispatcher.register(({ action }) => {
             break;
         case NAVIGATE_FORWARD:
             _.assign(formData, action.formData);
-            visitedSteps.push(getNextStep());
+            visitedSteps.push(moveToNextStep());
             break;
         case NAVIGATE_TO:
             visitedSteps = _.slice(visitedSteps, 0, _.indexOf(visitedSteps, action.step) + 1);
